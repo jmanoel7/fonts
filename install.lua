afd52c9360248dd895c93d003b2ba839b47e0777
João Manoel <joaomanoel7.dev@gmail.com>
2025-10-21 13:38:28 -0300

    atualizando script 00

--- install.lua
+++ install.lua
@@ -1,7 +1,7 @@
 -- local install = "mv fonts ~/.local/share/"
 
 function fn_install()
-  local install = "mv fonts ~/.local/share/"
+  local install = "cp -rf fonts/* ~/.local/share/fonts/"
   os.execute(install)
   print("Fonts to be read...")
   return true
